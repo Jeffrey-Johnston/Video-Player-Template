30cfce06a5499e6a42c85ecfa94a1223750a6faf
Jeffrey Johnston <jjohnston@nexient.com>
2021-08-26 18:07:00 -0400

    initial-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideoPlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/video-player/video-player.component.ts
@@ -0,0 +1,72 @@
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss'],
+})
+export class VideoPlayerComponent implements OnInit {
+  isPlay: boolean = false;
+  percentage!: number;
+  totalSeconds!: number;
+  videoSeconds!: number;
+  videoMinutes!: number;
+  minutes!: number;
+  seconds!: number;
+  progress!: number;
+  video!: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.video = document.getElementById('videoPlayer');
+    this.progressStatus();
+  }
+
+  playPause() {
+    if (this.video.paused) {
+      this.video.play();
+    } else {
+      this.video.pause();
+    }
+  }
+
+  mute() {
+    this.video.muted = !this.video.muted;
+  }
+
+  progressStatus() {
+    this.totalSeconds = this.video.currentTime;
+    this.minutes = Math.floor(this.totalSeconds / 60);
+    if (this.totalSeconds >= 60) {
+      this.seconds = Math.floor(this.totalSeconds - this.minutes * 60);
+    } else {
+      this.seconds = Math.floor(this.totalSeconds);
+    }
+
+    if (this.video.currentTime === 0) {
+      this.progress = 0;
+    } else {
+      this.progress = this.video.currentTime / this.video.duration;
+    }
+  }
+
+  setVideoLength() {
+    this.videoMinutes = Math.floor(this.video.duration / 60);
+    if (this.video.duration >= 60) {
+      this.videoSeconds = Math.floor(
+        this.video.duration - this.videoMinutes * 60
+      );
+    } else {
+      this.videoSeconds = Math.round(this.video.duration);
+    }
+  }
+
+  fullScreen() {}
+}
