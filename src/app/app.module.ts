297379e8d9c22cc788029aa0f2ea988802f0ea32
Jeffrey Johnston <jjohnston@nexient.com>
2021-08-27 17:36:26 -0400

    rick-roll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, VideoPlayerComponent],
+  declarations: [AppComponent, VideoPlayerComponent],
   imports: [BrowserModule, NgbModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
